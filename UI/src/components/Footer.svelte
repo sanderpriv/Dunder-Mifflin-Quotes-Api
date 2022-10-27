b970670fd7d9e920536213edbb0dd594bdc07da8
ska <sanderkaasen@hotmail.com>
2022-10-27 21:14:45 +0200

    Removed unnecessary boilerplate svelte code

---
+++ UI/src/components/Footer.svelte
@@ -0,0 +1,14 @@
+
+<footer>
+	<a href="/about">About</a>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+</style>
---
+++ UI/src/components/Header.svelte
@@ -0,0 +1,14 @@
+
+<header>
+
+	<h1>
+		<a href="/">Header</a>
+	</h1>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: center;
+	}
+</style>
